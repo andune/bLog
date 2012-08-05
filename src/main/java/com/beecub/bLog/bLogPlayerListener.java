d1188d2276dbeab79371b20a3b9181a51d2d8e90
Mark Morgan <morganm.qc@gmail.com>
2012-08-04 18:50:56 -0700

    update to Bukkit 1.3.1 compatibility (AsyncPlayerChatEvent)

--- src/main/java/com/beecub/bLog/bLogPlayerListener.java
+++ src/main/java/com/beecub/bLog/bLogPlayerListener.java
@@ -5,7 +5,7 @@ import java.util.logging.Logger;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
 
@@ -46,7 +46,7 @@ public class bLogPlayerListener implements Listener {
     }
 	
 	@EventHandler(priority=EventPriority.LOW)
-	public void onPlayerChat(PlayerChatEvent event) {
+	public void onPlayerChat(AsyncPlayerChatEvent event) {
 		if (event.isCancelled()) {
             return;
         }
